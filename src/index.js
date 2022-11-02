fc7708c55203c2030c9b732daa979c6ff0c6bf87
Peter Myer <peter.sr.myer@gmail.com>
2022-11-02 16:09:42 -0500

    adding new version of site before attempting to host

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  HashRouter as Router,
+  Route
+} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router
+     basename='/'>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
