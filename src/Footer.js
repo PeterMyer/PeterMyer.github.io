68d437227e11eccd3ac083b2df4ffc0a171f3ea9
Peter Myer <peter.sr.myer@gmail.com>
2022-11-02 15:09:36 -0500

    adjust formatting and update hashroutes

--- src/Footer.js
+++ src/Footer.js
@@ -1,21 +1,21 @@
 export function Footer(){
     return(
-    <body class="d-flex flex-column">
+    <div class="d-flex flex-column">
     <div id="page-content">
         <div class="container text-center">
         <div class="row justify-content-center">
             <div class="col-md-7">
-            <h1 class="fw-light mt-4 text-white">Sticky Footer using Flexbox</h1>
-            <p class="lead text-white-50">Use just two Bootstrap utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!</p>
+            <h1 class="fw-light mt-4 text-white"></h1>
+            <p class="lead text-white-50"></p>
             </div>
         </div>
         </div>
     </div>
     <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
         <div class="container text-center">
-        <small>Copyright &copy; Your Website</small>
+        <small>Copyright &copy; Peter Myer 2022</small>
         </div>
     </footer>
-    </body>
+    </div>
     )
 }
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Home'
 import About from "./About";
 import Projects from "./Projects";
@@ -11,11 +11,12 @@ export default function AppRoutes(){
     return(
         <div>
             <Routes>
-                <Route path="https://petermyer.github.io/" element={<Home/>} />
-                <Route path="https://petermyer.github.io/about" element={<About/>} />
+                <Route path="/about" element={<About/>} />
                 {/* <Route path="/BookIt" element={<BookItProjectPage/>} />
                 <Route path="/byte" element={<ByteProjectPage/>} /> */}
-                <Route path="https://petermyer.github.io/projects" element={<Projects/>} />
+                <Route path="/projects" element={<Projects/>} />
+                <Route path="/" element={<Home/>} />
+
             </Routes>
         </div>
     )
