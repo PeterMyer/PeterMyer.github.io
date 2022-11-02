6f33a117751b0039ae3b69f8d9089efa077f4e1a
Peter Myer <peter.sr.myer@gmail.com>
2022-11-02 13:11:36 -0500

    updated router to hashrouter

--- src/Routes.js
+++ src/Routes.js
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Home'
 import About from "./About";
 import Projects from "./Projects";
-import {BookItProjectPage} from "./BookItProjectPage";
-import {ByteProjectPage} from "./ByteProjectPage"
+// import {BookItProjectPage} from "./BookItProjectPage";
+// import {ByteProjectPage} from "./ByteProjectPage"
 
 export default function AppRoutes(){
 
@@ -16,7 +16,6 @@ export default function AppRoutes(){
                 {/* <Route path="/BookIt" element={<BookItProjectPage/>} />
                 <Route path="/byte" element={<ByteProjectPage/>} /> */}
                 <Route path="/projects" element={<Projects/>} />
-
             </Routes>
         </div>
     )
