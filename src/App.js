015b25437b740dde8e91c0106877da83f3e12545
Peter Myer <peter.sr.myer@gmail.com>
2022-11-30 21:39:08 -0600

    added google analytics code

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import NavBar from './Navbar';
 import AppRoutes from './Routes';
 import { Footer } from './Footer';
+import TagManager from 'react-gtm-module'
 
 function App() {
+
+  const tagManagerArgs = {
+    gtmId: 'GTM-5VW8PXG'
+  }
+  TagManager.initialize(tagManagerArgs)
+
   return (
     <div >
       <header className='container-fluid'>
